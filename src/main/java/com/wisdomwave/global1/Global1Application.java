525d56eee1773d7de80fa73c58d9133b104e50f5
FaithAdhiambo456 <156072384+FaithAdhiambo456@users.noreply.github.com>
2024-09-18 18:01:17 +0300

    first commit

---
+++ src/main/java/com/wisdomwave/global1/Global1Application.java
@@ -0,0 +1,13 @@
+package com.wisdomwave.global1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Global1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Global1Application.class, args);
+	}
+
+}
